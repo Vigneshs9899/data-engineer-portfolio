a5651ef9cdc05f9c1079c1abcb91f015d93c3852
vignesh <vigneshs9899@gmail.com>
2026-03-29 08:39:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+function App() {
+  return (
+    <div>
+
+      <h1>VIGNESH S</h1>
+      <h2>Azure Data Engineer</h2>
+
+      <section>
+        <h2>About Me</h2>
+        <p>
+        Data Engineer specializing in Azure data platforms,
+        ETL pipelines and big data processing.
+        </p>
+      </section>
+
+      <section>
+        <h2>Skills</h2>
+
+        <ul>
+          <li>Python</li>
+          <li>SQL</li>
+          <li>Azure Data Factory</li>
+          <li>Databricks</li>
+          <li>Spark</li>
+          <li>Azure Synapse</li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Projects</h2>
+
+        <h3>Azure Data Pipeline</h3>
+        <p>
+        Built an end-to-end pipeline using Azure Data Factory,
+        Data Lake and Databricks.
+        </p>
+
+        <h3>Real Time Data Pipeline</h3>
+        <p>
+        Kafka + Spark Streaming + Delta Lake architecture.
+        </p>
+
+      </section>
+
+      <section>
+        <h2>Contact</h2>
+        <p>Email: vigneshsaravanan.dev@outlook.com</p>
+        <p>GitHub: github.com/Vigneshs9899</p>
+      </section>
+
+    </div>
+  );
+}
+
+export default App;
